457156972d8b3e0d934be9eae74c6743e9c9a81c
PoorlyDefinedBehaviour <brunotj2015@hotmail.com>
2024-12-24 14:51:47 -0300

    break money transfer into 2 steps

--- src/money_transfer.rs
+++ src/money_transfer.rs
@@ -3,6 +3,7 @@ use std::collections::HashMap;
 #[derive(Debug, Clone)]
 struct State {
     balances: HashMap<String, i64>,
+    pc: HashMap<i64, String>,
 }
 
 impl std::hash::Hash for State {
@@ -13,6 +14,13 @@ impl std::hash::Hash for State {
             key.hash(state);
             self.balances.get(key).unwrap().hash(state);
         }
+
+        let mut keys: Vec<_> = self.pc.keys().collect();
+        keys.sort_unstable();
+        for key in keys {
+            key.hash(state);
+            self.pc.get(key).unwrap().hash(state);
+        }
     }
 }
 
@@ -21,26 +29,34 @@ fn deposit(s: &mut State, account: &str, amount: i64) {
     *balance += amount;
 }
 
-fn transfer(s: &mut State, from: &str, to: &str, amount: i64) {
-    let balance = s.balances.get_mut(from).unwrap();
-    *balance -= amount;
+fn start_transfer(s: &mut State, r: i64, from: &str, to: &str, amount: i64) {
+    if s.pc.get(&r).unwrap() != "start_transfer" {
+        return;
+    }
 
-    let balance = s.balances.get_mut(to).unwrap();
-    *balance += amount;
+    let balance = s.balances.get_mut(from).unwrap();
+    if *balance - amount >= 0 {
+        s.pc.insert(r, "complete_transfer".to_owned());
+    }
+}
+
+fn complete_transfer(s: &mut State, r: i64, from: &str, to: &str, amount: i64) {
+    if s.pc.get(&r).unwrap() != "complete_transfer" {
+        return;
+    }
+
+    *s.balances.get_mut(from).unwrap() -= amount;
+    *s.balances.get_mut(to).unwrap() += amount;
+
+    s.pc.insert(r, "done".to_owned());
 }
 
 fn consistency(s: &State) -> bool {
-    if *s.balances.get("Alice").unwrap() == 1 && *s.balances.get("Bob").unwrap() == 3 {
-        return false;
-    }
-
-    true
+    s.balances.iter().all(|(_, balance)| *balance >= 0)
 }
 
 #[cfg(test)]
 mod tests {
-    use std::collections::HashSet;
-
     use crate::model_checker::ModelChecker;
 
     use super::*;
@@ -55,18 +71,20 @@ mod tests {
                     deposit(s, account, 1)
                 })
             })
-            .action("transfer", |c, s| {
-                let accounts: HashSet<_> = s.balances.keys().collect();
-                c.there_exists(accounts.iter(), |_, from| {
-                    c.there_exists(accounts.iter().filter(|acc| acc != &from), |s, to| {
-                        transfer(s, from, to, 1)
-                    })
-                });
+            .action("start_transfer", |c, s| {
+                c.there_exists(1..=2, |s, r| start_transfer(s, r, "Alice", "Bob", 1));
+            })
+            .action("complete_transfer", |c, s: &mut State| {
+                c.there_exists(1..=2, |s, r| complete_transfer(s, r, "Alice", "Bob", 1));
             })
             .invariant("consistency", consistency);
 
         checker.run(State {
-            balances: HashMap::from([("Alice".to_string(), 2), ("Bob".to_string(), 1)]),
+            balances: HashMap::from([("Alice".to_string(), 1), ("Bob".to_string(), 1)]),
+            pc: HashMap::from([
+                (1, "start_transfer".to_owned()),
+                (2, "start_transfer".to_owned()),
+            ]),
         });
     }
 }
